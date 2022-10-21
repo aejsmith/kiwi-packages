35953764a424f1022606cc673e23db1d8e67a7d9
Alex Smith <alex@alex-smith.me.uk>
2022-10-21 18:56:26 +0100

    Package: freetype-2.12.1-1

---
+++ source/freetype/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+name="freetype"
+version="2.12.1"
+revision="1"
+
+tar xf "${name}-${version}.tar.xz"
+patch -d "${name}-${version}" -Np1 -i "../${name}-${version}-kiwi.patch"
+
+mkdir build install
+cd build
+
+"../${name}-${version}/configure" --host=x86_64-kiwi --prefix=/system --bindir=/system/bin --libdir=/system/lib --includedir=/system/devel/include --datadir=/system/data/freetype --libexecdir=/system/lib --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no --with-brotli=no --enable-static=no
+
+sed -i 's|/\* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING \*/|#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING|' ftoption.h
+
+make -j8
+make DESTDIR=$(cd ../install; pwd) install
+
+cd ..
+
+tar -cvJf "${name}-${version}-${revision}.bin.tar.xz" -C install .
+
+rm -rf build install "${name}-${version}"
