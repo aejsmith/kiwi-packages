ca17c5c061ca3f2884944d6c1bb1271ee0b08e93
Alex Smith <alex@alex-smith.me.uk>
2022-10-22 09:03:08 +0100

    Package: freetype-2.12.1-2
    Rebuilt against our own zlib package.

--- source/freetype/build.sh
+++ source/freetype/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 name="freetype"
 version="2.12.1"
-revision="1"
+revision="2"
 
 tar xf "${name}-${version}.tar.xz"
 patch -d "${name}-${version}" -Np1 -i "../${name}-${version}-kiwi.patch"
@@ -9,7 +9,7 @@ patch -d "${name}-${version}" -Np1 -i "../${name}-${version}-kiwi.patch"
 mkdir build install
 cd build
 
-"../${name}-${version}/configure" --host=x86_64-kiwi --prefix=/system --bindir=/system/bin --libdir=/system/lib --includedir=/system/devel/include --datadir=/system/data/freetype --libexecdir=/system/lib --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no --with-brotli=no --enable-static=no
+"../${name}-${version}/configure" --host=x86_64-kiwi --prefix=/system --bindir=/system/bin --libdir=/system/lib --includedir=/system/devel/include --datadir=/system/data/freetype --libexecdir=/system/lib --with-zlib=yes --with-bzip2=no --with-png=no --with-harfbuzz=no --with-brotli=no --enable-static=no
 
 sed -i 's|/\* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING \*/|#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING|' ftoption.h
 
