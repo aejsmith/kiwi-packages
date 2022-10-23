b9f5e9bf1988bd8622f10fffe20ae7bdb691a8b3
Alex Smith <alex@alex-smith.me.uk>
2022-10-23 09:19:47 +0100

    Package: curl-7.85.0-1

---
+++ source/curl/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+name="curl"
+version="7.85.0"
+revision="1"
+
+tar xf "${name}-${version}.tar.xz"
+patch -d "${name}-${version}" -Np1 -i "../${name}-${version}-kiwi.patch"
+
+mkdir build install
+cd build
+
+# TODO: Enable socketpair when we support it and AF_UNIX sockets.
+"../${name}-${version}/configure" --host=x86_64-kiwi --prefix=/system --bindir=/system/bin --libdir=/system/lib --includedir=/system/devel/include --datadir=/system/data/curl --libexecdir=/system/lib --enable-static=no --without-ssl --disable-ipv6 --disable-socketpair
+make -j8
+make DESTDIR=$(cd ../install; pwd) install
+
+cd ..
+
+rm -rf install/system/share
+
+tar -cvJf "${name}-${version}-${revision}.bin.tar.xz" -C install .
+
+rm -rf build install "${name}-${version}"
