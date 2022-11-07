f3fc70825ec4d3a618d7315b4de0e7a89d43fec7
Alex Smith <alex@alex-smith.me.uk>
2022-11-07 17:37:31 +0000

    Package: nano-6.4-2
    Correct data directory (it puts its data in a nano subdirectory without us
    needing to do add that to the path when building).

--- source/nano/build.sh
+++ source/nano/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 name="nano"
 version="6.4"
-revision="1"
+revision="2"
 
 tar xf "${name}-${version}.tar.xz"
 
@@ -11,7 +11,7 @@ mkdir build install
 cd build
 
 # TODO: Remove LIBS once we link to libkernel by default.
-LIBS="-lkernel" "../${name}-${version}/configure" --host=x86_64-kiwi --prefix=/system --datadir=/system/data/nano --libexecdir=/system/lib --disable-mouse --disable-speller --disable-nls --disable-utf8 ac_cv_header_langinfo_h=0
+LIBS="-lkernel" "../${name}-${version}/configure" --host=x86_64-kiwi --prefix=/system --datadir=/system/data --libexecdir=/system/lib --disable-mouse --disable-speller --disable-nls --disable-utf8 ac_cv_header_langinfo_h=0
 make -j8
 make DESTDIR="$(cd ../install; pwd)" install
 
